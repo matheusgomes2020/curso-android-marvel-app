af502874deab5c833783fb15820b959a7b345184
matheusgomes2020 <matheusgomez2013@gmail.com>
2024-03-29 16:15:18 -0300

    corrige teste unitário do view model

--- app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
+++ app/src/androidTest/java/com/example/marvelapp/presentation/characters/CharactersFragmentTest.kt
@@ -1,12 +1,9 @@
 package com.example.marvelapp.presentation.characters
 
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.marvelapp.R
 import com.example.marvelapp.extension.asJsonString
@@ -18,7 +15,6 @@ import dagger.hilt.android.testing.UninstallModules
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.After
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
